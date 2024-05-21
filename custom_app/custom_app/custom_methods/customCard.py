91904de1999240acd4bdef664bc2a3e4a6b42612
Aman Agnihotri <69078309+amanagnihotri1@users.noreply.github.com>
2024-05-21 23:52:08 +0530

    Update customCard.py

--- custom_app/custom_app/custom_methods/customCard.py
+++ custom_app/custom_app/custom_methods/customCard.py
@@ -1,13 +1,13 @@
 import frappe
 @frappe.whitelist()
-def get_customer_count():
+def get_customer_count(): # function to get count of customer present in customer doctype
     data = frappe.db.sql("""SELECT COUNT(*) FROM `tabCustomer`;""")
     return {     
 	"value": data,
 	"fieldtype": "Int",
     }
 @frappe.whitelist()
-def get_active_students():
+def get_active_students(): # function to get count of active students from custom doctype based on few given filters
     students=frappe.db.sql("""SELECT COUNT(*) FROM `tabStudent`
                           WHERE active_check=1 AND stu_age>=18
                           """)
